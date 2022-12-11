816dc4a9e430c6a1babbf48acd02b4cd89675094
SmaginDima <smaginzdima@gmail.com>
2022-12-11 20:38:27 +0300

    Задача 29

---
+++ n3sem4/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив
+// из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+Console.WriteLine("Введите числа массива");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int x3 = Convert.ToInt32(Console.ReadLine());
+int x4 = Convert.ToInt32(Console.ReadLine());
+int x5 = Convert.ToInt32(Console.ReadLine());
+int x6 = Convert.ToInt32(Console.ReadLine());
+int x7 = Convert.ToInt32(Console.ReadLine());
+int x8 = Convert.ToInt32(Console.ReadLine());
+int[] array = {x1,x2,x3,x4,x5,x6,x7,x8};
+    Console.WriteLine();
+
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(array);
